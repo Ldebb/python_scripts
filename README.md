c621ecbad9690ed5a46d1eeb59e97efd662140c7
DEBBICHI <debbichilamjed@yahoo.fr>
2019-11-11 00:04:06 +0100

    change the list

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # python_scripts
 
 Here scripts to remove duplicate with python.
+We add somme feature to the script.
--- duplicate.py
+++ duplicate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-list =[4, 5, 7, 8, 9, 1, 0, 7, 4, 1]
+list =[4, 5, 7, 8, 9, 1, 0, 7, 4, 1, 4, 4, 7, 7, 2, 1, 8, 8]
 list.sort()
 prev=list[0]
 del list[0]
